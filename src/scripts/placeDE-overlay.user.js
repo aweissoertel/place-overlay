8a9cea8efa01f20e0b98c4a518c020dd68a3686c
aweissoertel <alex.weissoertel@t-online.de>
2023-07-22 00:01:47 +0200

    fix script

--- src/scripts/placeDE-overlay.user.js
+++ src/scripts/placeDE-overlay.user.js
@@ -6,8 +6,8 @@
 // @author       alex
 // @match        https://garlic-bread.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
-// @downloadURL  https://github.com/PlaceDE-Official/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
+// @updateURL    https://github.com/aweissoertel/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
+// @downloadURL  https://github.com/aweissoertel/place-overlay/raw/main/src/scripts/placeDE-overlay.user.js
 // ==/UserScript==
 
 var overlayImage = null;
